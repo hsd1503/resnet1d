2287c8271035149ecda639d60560ef1b501c3b24
Shenda Hong <hongshenda@pku.edu.cn>
2019-12-07 14:51:33 -0500

    fix data path

--- util.py
+++ util.py
@@ -9,27 +9,28 @@ from tqdm import tqdm
 
 def preprocess_physionet():
     """
-    download the raw data from https://physionet.org/content/challenge-2017/1.0.0/
+    download the raw data from https://physionet.org/content/challenge-2017/1.0.0/, 
+    and put it in ../data/challenge2017/
     """
     
     # read label
-    label_df = pd.read_csv('challenge2017/REFERENCE-v3.csv', header=None)
+    label_df = pd.read_csv('../data/challenge2017/REFERENCE-v3.csv', header=None)
     label = label_df.iloc[:,1].values
     print(Counter(label))
 
     # read data
     all_data = []
-    filenames = pd.read_csv('challenge2017/training2017/RECORDS', header=None)
+    filenames = pd.read_csv('../data/challenge2017/training2017/RECORDS', header=None)
     filenames = filenames.iloc[:,0].values
     print(filenames)
     for filename in tqdm(filenames):
-        mat = scipy.io.loadmat('challenge2017/training2017/{0}.mat'.format(filename))
+        mat = scipy.io.loadmat('../data/challenge2017/training2017/{0}.mat'.format(filename))
         mat = np.array(mat['val'])[0]
         all_data.append(mat)
     all_data = np.array(all_data)
 
     res = {'data':all_data, 'label':label}
-    with open('challenge2017/challenge2017.pkl', 'wb') as fout:
+    with open('../data/challenge2017/challenge2017.pkl', 'wb') as fout:
         pickle.dump(res, fout)
 
 def slide_and_cut(X, Y, window_size, stride, output_pid=False):
