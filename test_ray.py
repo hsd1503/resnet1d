751b79089fc2789620a9f3394f6b8b531e475e84
Shenda Hong <hongshenda@pku.edu.cn>
2019-12-28 16:56:53 -0500

    model profile

--- test_ray.py
+++ test_ray.py
@@ -62,11 +62,18 @@ def test(model, device, test_loader, label_test):
     print(classification_report(all_pred, label_test))
 
 class Network(object):
-    def __init__(self):
+    def __init__(self, n_length, base_filters, kernel_size, n_block):
+        """
+        key parameters to control the model:
+            n_length: dimention of input (resolution) [16, 64, 256, 1024, 4096]
+            base_filters: number of convolutional filters (width) [8, 16, 32, 64, 128]
+            kernel_size: size of convolutional filters [2, 4, 8, 16]
+            n_block: depth of model (depth) [2, 4, 8, 16]
+        """
         use_cuda = torch.cuda.is_available()
 
         n_samples = 1000
-        n_length = 7500
+        n_length = n_length
         n_channel = 1
         n_classes = 2
 
@@ -85,19 +92,19 @@ class Network(object):
         self.train_loader, self.test_loader = dataloader, dataloader_test
 
         ## change the hyper-parameters for your own data
-        # (n_block, downsample_gap, increasefilter_gap) = (8, 1, 2)
+        # recommend: (n_block, downsample_gap, increasefilter_gap) = (8, 1, 2)
         # 34 layer (16*2+2): 16, 2, 4
         # 98 layer (48*2+2): 48, 6, 12
         self.model = ResNet1D(
                     in_channels=n_channel, 
-                    base_filters=64, 
-                    kernel_size=16, 
+                    base_filters=base_filters, 
+                    kernel_size=kernel_size, 
                     stride=2, 
-                    n_block=16, 
-                    groups=32,
+                    n_block=n_block, 
+                    groups=base_filters,
                     n_classes=n_classes, 
-                    downsample_gap=2, 
-                    increasefilter_gap=4, 
+                    downsample_gap=max(n_block//8, 1), 
+                    increasefilter_gap=max(n_block//4, 1), 
                     verbose=False).to(device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
 
@@ -117,27 +124,50 @@ class Network(object):
 
 if __name__ == "__main__":
 
-    ray.init()
-    RemoteNetwork = ray.remote(num_gpus=2)(Network)
+    # ------------------ test make model ------------------
 
-    NetworkActor = RemoteNetwork.remote()
-    NetworkActor2 = RemoteNetwork.remote()
-    ray.get([NetworkActor.train.remote(), NetworkActor2.train.remote()])
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    for n_length in [16, 64, 256, 1024, 4096]:
+        data, label = read_data_generated(n_samples=1000, n_length=n_length, n_channel=1, n_classes=2)
+        for base_filters in [8, 16, 32, 64, 128]:
+            for kernel_size in [2, 4, 8, 16]:
+                for n_block in [2, 4, 8, 16]:
+                    print('\n'*3)
+                    print('*'*60)
+                    print('*'*60)
+                    print("n_length: {}, base_filters: {}, kernel_size: {}, n_block: {}".format(n_length, base_filters, kernel_size, n_block))
+                    print('*'*60)
+                    print('*'*60)
+                    net = Network(
+                        n_length=n_length, 
+                        base_filters=base_filters, 
+                        kernel_size=kernel_size, 
+                        n_block=n_block)
+                    net.model.to(device)
+                    summary(net.model, (data.shape[1], data.shape[2]))
 
-    weights = ray.get(
-        [NetworkActor.get_weights.remote(),
-        NetworkActor2.get_weights.remote()])
+    # ------------------ test ray ------------------
+    # ray.init()
+    # RemoteNetwork = ray.remote(num_gpus=2)(Network)
 
-    from collections import OrderedDict
-    averaged_weights = OrderedDict(
-        [(k, (weights[0][k] + weights[1][k]) / 2) for k in weights[0]])
+    # NetworkActor = RemoteNetwork.remote()
+    # NetworkActor2 = RemoteNetwork.remote()
+    # ray.get([NetworkActor.train.remote(), NetworkActor2.train.remote()])
 
-    weight_id = ray.put(averaged_weights)
-    [
-        actor.set_weights.remote(weight_id)
-        for actor in [NetworkActor, NetworkActor2]
-    ]
-    ray.get([actor.train.remote() for actor in [NetworkActor, NetworkActor2]])
+    # weights = ray.get(
+    #     [NetworkActor.get_weights.remote(),
+    #     NetworkActor2.get_weights.remote()])
+
+    # from collections import OrderedDict
+    # averaged_weights = OrderedDict(
+    #     [(k, (weights[0][k] + weights[1][k]) / 2) for k in weights[0]])
+
+    # weight_id = ray.put(averaged_weights)
+    # [
+    #     actor.set_weights.remote(weight_id)
+    #     for actor in [NetworkActor, NetworkActor2]
+    # ]
+    # ray.get([actor.train.remote() for actor in [NetworkActor, NetworkActor2]])
 
 
 
